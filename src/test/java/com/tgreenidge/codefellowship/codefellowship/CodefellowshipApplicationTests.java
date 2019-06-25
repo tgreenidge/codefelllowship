cdefee85af2679a39cd276086c2d2157762668ac
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-06-25 06:28:55 -0700

    add tests

--- src/test/java/com/tgreenidge/codefellowship/codefellowship/CodefellowshipApplicationTests.java
+++ src/test/java/com/tgreenidge/codefellowship/codefellowship/CodefellowshipApplicationTests.java
@@ -1,16 +1,59 @@
 package com.tgreenidge.codefellowship.codefellowship;
 
+
+
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.*;
+
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class CodefellowshipApplicationTests {
+    @Autowired
+    CodefellowshipController codefellowshipController;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void testControllerIsAutowired() {
+        assertNotNull(codefellowshipController);
+    }
+
+    @Test
+    public void testRequestToRootGivesWelcome() throws Exception {
+        mockMvc.perform(get("/")).andExpect(content().string(containsString("Welcome to Codefellowship!")));
+    }
+
+    @Test
+    public void testRequestToSignUpGivesSignUp() throws Exception {
+        mockMvc.perform(get("/signup")).andExpect(content().string(containsString("Sign up")));
+    }
+
+    @Test
+    public void testRequestToLoginGivesUsername() throws Exception {
+        mockMvc.perform(get("/login")).andExpect(content().string(containsString("Username:")));
+    }
 
 }
+
